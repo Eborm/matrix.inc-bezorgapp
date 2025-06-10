8a2f94cb1ff88faabd8e501e1cbd6e8389181191
Eborm <bramwesel@gmail.com>
2025-06-10 11:07:54 +0200

    maui project toegevoegd

---
+++ bezorgapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace bezorgapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ bezorgapp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace bezorgapp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
