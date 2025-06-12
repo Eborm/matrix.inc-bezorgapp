7adc49bdc90beaf0b3690744e0fcb106e339a4af
Daan Ros <daanros@Macbook-Air-van-Daan.local>
2025-06-12 20:53:37 +0200

    Eerste versie foto's in app toegevoegd

---
+++ bezorgapp/PhotoListPage.xaml.cs
@@ -0,0 +1,44 @@
+using System.Net.Http.Json;
+
+namespace bezorgapp
+{
+    public partial class PhotoListPage : ContentPage
+    {
+        public PhotoListPage()
+        {
+            InitializeComponent();
+            LoadPhotosAsync();
+        }
+        
+        private async void OnViewPhotosClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PhotoListPage());
+        }
+
+        private async void LoadPhotosAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var photos = await client.GetFromJsonAsync<List<string>>("https://bezorgapp-api-1234.azurewebsites.net/api/upload/list");
+
+                if (photos != null)
+                {
+                    foreach (var url in photos)
+                    {
+                        Image image = new Image
+                        {
+                            Source = ImageSource.FromUri(new Uri(url)),
+                            HeightRequest = 200
+                        };
+                        ImageList.Children.Add(image);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Fout", $"Kan foto's niet laden: {ex.Message}", "OK");
+            }
+        }
+    }
+}
