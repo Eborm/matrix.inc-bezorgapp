fb243a038e0e706d6ce43c4405aa79263a2ae366
Eborm <bramwesel@gmail.com>
2025-06-17 11:34:04 +0200

    Added  - DeliveryService Model  - DeliveryState Model  - GetDeliveryStateByIdAsyc To get the delivery states for a Orderid

---
+++ bezorgapp/Models/DeliveryState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bezorgapp.Models
+{
+    public class DeliveryState
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int State { get; set; } // Bijvoorbeeld: 1 = In afwachting, 2 = Onderweg, 3 = Afgeleverd
+        public DateTime DateTime { get; set; }
+
+        public DeliveryService? DeliveryService { get; set; }
+        public Order? Order { get; set; }
+    }
+
+}
--- bezorgapp/Services/ApiService.cs
+++ bezorgapp/Services/ApiService.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using bezorgapp.Models;
 
 namespace bezorgapp.Services;
@@ -17,6 +18,38 @@ public class ApiService
         _httpClient = new HttpClient();
         _httpClient.BaseAddress = new Uri(_baseUrl);
         _httpClient.DefaultRequestHeaders.Add("apiKey", _apiKey);
+    }
+
+    private async Task<IEnumerable<DeliveryState>> GetDeliveryStateByIdAsync(int Id)
+    {
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("apiKey", _apiKey);
+        DeliveryState OrderDeliveryState;
+
+        try
+        {
+            var response = await httpClient.GetAsync($"{_baseUrl}/Api/DeliveryStates/GetAllDeliveryStates");
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var deliveryStates = JsonSerializer.Deserialize<List<DeliveryState>>(jsonResponse, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            var statesForOrder = deliveryStates
+                .Where(ds => ds.OrderId == Id)
+                .ToList();
+
+            string serialized = JsonSerializer.Serialize(statesForOrder, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            return statesForOrder;
+
+        }
+        catch (Exception ex)
+        {
+            return new List<DeliveryState>();   
+        }
     }
 
     public async Task<List<Order>> GetOrdersAsync()
