e8cf55dbf86ee551a7d1eff2cee6cab86c4f45b2
Daan Ros <2402709ros@zuyd.nl>
2025-06-17 09:59:13 +0200

    Settings page gemaakt

--- bezorgapp/MainPage.xaml.cs
+++ bezorgapp/MainPage.xaml.cs
@@ -5,20 +5,6 @@
         public MainPage()
         {
             InitializeComponent();
-            bool isDarkMode = Preferences.Get("DarkModeEnabled", false);
-            ThemeSwitch.IsToggled = isDarkMode;
-            if (Application.Current != null)
-            {
-                Application.Current.UserAppTheme = isDarkMode ? AppTheme.Dark : AppTheme.Light;
-            }
-        }
-        private void OnThemeToggled(object sender, ToggledEventArgs e)
-        {
-            if (Application.Current != null)
-            {
-                Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
-                Preferences.Set("DarkModeEnabled", e.Value);
-            }
         }
     }
 }
