ef3760db82d53a228b542451c584192f7199aece
Daan Ros <2402709ros@zuyd.nl>
2025-06-20 12:28:17 +0200

    Barcode scanner scant id en gaat naar pakketje en zet over naar volgende stap.

--- bezorgapp/BarCodeScannerPage.xaml.cs
+++ bezorgapp/BarCodeScannerPage.xaml.cs
@@ -1,16 +1,19 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using bezorgapp.Services;
+using bezorgapp.Models;
 
 namespace bezorgapp
 {
     public partial class BarcodeScannerPage : ContentPage
     {
+        private readonly ApiService _apiService;
+
         public BarcodeScannerPage()
         {
             InitializeComponent();
+            _apiService = new ApiService();
         }
 
         private void BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
@@ -19,21 +22,70 @@ namespace bezorgapp
             if (string.IsNullOrEmpty(eersteResultaat))
                 return;
             
-            Dispatcher.Dispatch(() =>
+            barcodeReader.IsDetecting = false;
+            
+            Dispatcher.Dispatch(async () =>
             {
-                barcodeReader.IsDetecting = false;
-                
-                resultLabel.Text = eersteResultaat;
-                
-                DisplayAlert("Barcode gescand", resultLabel.Text, "OK");
-                
+                loadingIndicator.IsVisible = true;
+
+                if (!int.TryParse(eersteResultaat, out int scannedOrderId))
+                {
+                    await DisplayAlert("Fout", "De gescande barcode is geen geldig ordernummer.", "OK");
+                    barcodeReader.IsDetecting = true;
+                    loadingIndicator.IsVisible = false;
+                    return;
+                }
+
+                var userOrders = await _apiService.GetOrdersAsync();
+                var foundOrder = userOrders.FirstOrDefault(o => o.Id == scannedOrderId);
+
+                if (foundOrder == null)
+                {
+                    await DisplayAlert("Niet gevonden", $"Order {scannedOrderId} is niet gevonden in jouw lijst.", "OK");
+                    barcodeReader.IsDetecting = true;
+                    loadingIndicator.IsVisible = false;
+                    return;
+                }
+
+                switch (foundOrder.DeliveryState)
+                {
+                    case "In afwachting":
+                        var (inProgressSuccess, inProgressError) = await _apiService.MarkAsInProgressAsync(foundOrder.Id);
+                        if (inProgressSuccess)
+                        {
+                            await DisplayAlert("Succes", $"Order {foundOrder.Id} is nu 'Onderweg'.", "OK");
+                            await Navigation.PopAsync();
+                        }
+                        else
+                        {
+                            await DisplayAlert("Fout", $"Kon status niet bijwerken: {inProgressError}", "OK");
+                        }
+                        break;
+
+                    case "Onderweg":
+                        await DisplayAlert("Info", "Order is al onderweg. Maak een foto om de aflevering te voltooien.", "OK");
+                        await Navigation.PushAsync(new CreatePicture(foundOrder.Id, true));
+                        break;
+
+                    default:
+                        await DisplayAlert("Info", $"Er is geen automatische actie voor order {foundOrder.Id} met status '{foundOrder.DeliveryState}'.", "OK");
+                        barcodeReader.IsDetecting = true;
+                        break;
+                }
+
+                loadingIndicator.IsVisible = false;
             });
+            // --- EINDE AANPASSING ---
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            barcodeReader.IsDetecting = true;
+            Dispatcher.Dispatch(() =>
+            {
+                barcodeReader.IsDetecting = true;
+                loadingIndicator.IsVisible = false;
+            });
         }
 
         protected override void OnDisappearing()
