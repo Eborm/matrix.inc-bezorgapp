42f7cb0064f772a51a8bc2f708d5e3aedbbcbe44
Daan Ros <2402709ros@zuyd.nl>
2025-06-24 15:20:45 +0200

    Terugknop eindelijk gemaakt🥳

--- bezorgapp/AppShell.xaml.cs
+++ bezorgapp/AppShell.xaml.cs
@@ -1,16 +1,17 @@
-﻿// AppShell.xaml.cs
-// Deze klasse definieert de navigatiestructuur van de applicatie.
-
-namespace bezorgapp
+﻿namespace bezorgapp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            // Registreer routes voor navigatie naar pagina's binnen de app
-            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            
+            Routing.RegisterRoute(nameof(OrdersPage), typeof(OrdersPage));
             Routing.RegisterRoute(nameof(PhotoGalleryPage), typeof(PhotoGalleryPage));
+            Routing.RegisterRoute(nameof(PhotoDetailPage), typeof(PhotoDetailPage));
+            Routing.RegisterRoute(nameof(CreatePicture), typeof(CreatePicture));
+            Routing.RegisterRoute(nameof(BarcodeScannerPage), typeof(BarcodeScannerPage));
+            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
         }
     }
 }
