253f6cfe752badce0c5a1403bbe89b2229b4f56c
Eborm <bramwesel@gmail.com>
2025-06-27 12:19:49 +0200

    route wordt nu geoptimalizeerd en opent de voledige route

--- bezorgapp/MainPage.xaml.cs
+++ bezorgapp/MainPage.xaml.cs
@@ -38,12 +38,19 @@ namespace bezorgapp
 
             // Define coordinates
             string start_end = "50.85056910665486,6.011646432851468"; // Warhouse
-            string waypoint1 = "50.892543417163814,5.98174669784661";
-            string waypoint2 = "50.882447992696584,5.98098648461065";
+            string waypoint1 = "";
+            string waypoint2 = "";
+            string waypoint3 = ""; 
+            List<(double lon, double lat)> cords = new List<(double lon, double lat)>() { _mapService.AdressToCoordinates("123 Elm St")[0], _mapService.AdressToCoordinates("456 Oak St")[0], _mapService.AdressToCoordinates("789 Pine St")[0] };
+            var optimizedRoute = await _mapService.OptimizeRouteAsync(cords);
+            waypoint1 += optimizedRoute[0].lat.ToString(CultureInfo.InvariantCulture) + "," + optimizedRoute[0].lon.ToString(CultureInfo.InvariantCulture);
+            waypoint2 += optimizedRoute[1].lat.ToString(CultureInfo.InvariantCulture) + "," + optimizedRoute[1].lon.ToString(CultureInfo.InvariantCulture);
+            waypoint3 += optimizedRoute[2].lat.ToString(CultureInfo.InvariantCulture) + "," + optimizedRoute[2].lon.ToString(CultureInfo.InvariantCulture);
+
 
             // Build Google Maps directions URL
-            string url = $"https://www.google.com/maps/dir/?api=1&origin={origin}&destination={start_end}&waypoints={start_end}|{waypoint1}|{waypoint2}&travelmode=driving";
-
+            string url = $"https://www.google.com/maps/dir/?api=1&origin={origin}&destination={start_end}&waypoints={start_end}|{waypoint1}|{waypoint2}|{waypoint3}&travelmode=driving";
+            Debug.WriteLine(url);
             await Launcher.Default.OpenAsync(url);
         }
     }
