928141a2c6151e9b483beccd177ef688927fc314
Daan Ros <2402709ros@zuyd.nl>
2025-06-28 16:47:32 +0200

    Models opgeruimd

--- bezorgapp/Models/Customer.cs
+++ bezorgapp/Models/Customer.cs
@@ -5,15 +5,9 @@ namespace bezorgapp.Models;
 
 public class Customer
 {
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
-
     [JsonPropertyName("name")]
     public string Name { get; set; }
 
     [JsonPropertyName("address")]
     public string Address { get; set; }
-
-    [JsonPropertyName("active")]
-    public bool Active { get; set; }
 }
--- bezorgapp/Models/Order.cs
+++ bezorgapp/Models/Order.cs
@@ -6,27 +6,8 @@ public class Order
 {
     [JsonPropertyName("id")]
     public int Id { get; set; }
-
-    [JsonPropertyName("orderDate")]
-    public DateTime OrderDate { get; set; }
-
-    [JsonPropertyName("customerId")]
-    public int CustomerId { get; set; }
-
+    
     [JsonPropertyName("customer")]
     public Customer Customer { get; set; }
-
-    [JsonPropertyName("products")]
-    public List<object> Products { get; set; }
-
-    [JsonPropertyName("deliveryStates")]
-    public List<object> DeliveryStates { get; set; }
-
-    public int DeliveryStateState { get; set; } // 0 = In afwachting, 1 = Onderweg, 2 = Afgeleverd
-
     public string DeliveryState { get; set; }
-    
-    public string DeliveryServiceName { get; set; }
-    
-    public int DeliveryServiceId { get; set; }
 }
